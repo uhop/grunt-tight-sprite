e49b39bb1b75c0f8c47c029b6a0f8656c5ca7b6d
Eugene Lazutkin <eugene@lazutkin.com>
2013-11-18 23:56:17 -0600

    Implemented algorithms with immutable points.

--- src/Envelope.js
+++ src/Envelope.js
@@ -13,26 +13,17 @@ function Envelope(){
 
 Envelope.prototype = {
 	add: function(index, rect){
-		// This function mutates an array, and its objects.
-		// It is possible to make objects immutable.
-		// Potentially, it may reduce the memory load.
 		var cp = this.cornerPoints, p = cp[index],
 			br = {x: p.x + rect.w, y: p.y + rect.h};
 		rect.x = p.x;
 		rect.y = p.y;
 		var i1 = searchVertically(cp, br, 0, index),
 			i2 = searchHorizontally(cp, br, index + 1),
-			next = i1 + 1, len = i2 - next;
-		cp[i1].y = br.y;
+			len = i2 - i1;
 		if(i2 < cp.length && cp[i2].x == br.x){
-			cp.splice(next, len);
+			cp.splice(i1, len, {x: cp[i1].x, y: br.y});
 		}else{
-			if(len > 0){
-				cp.splice(next, len - 1);
-				cp[next].x = br.x;
-			}else{
-				cp.splice(next, 0, {x: br.x, y: rect.y});
-			}
+			cp.splice(i1, len, {x: cp[i1].x, y: br.y}, {x: br.x, y: i2 < cp.length ? rect.y : 0});
 		}
 		return this;
 	},
@@ -46,12 +37,7 @@ Envelope.prototype = {
 		return area;
 	},
 	clone: function(){
-		// The next one is a candidate for heya-pipe.
-		// All it does it clones an array and its objects.
-		// If we make objects immutable, we can just slice an array.
-		return this.cornerPoints.map(function(p){
-			return {x: p.x, y: p.y};
-		});
+		return this.cornerPoints.slice(0);
 	}
 };
 
