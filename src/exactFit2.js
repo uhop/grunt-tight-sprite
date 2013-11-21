17483a400952b1ebf30eebb6f72f129cbd2b22d3
Eugene Lazutkin <eugene@lazutkin.com>
2013-11-21 17:31:31 -0600

    New exactFit() based on lookAhead().

---
+++ src/exactFit2.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+var Envelope  = require("./Envelope");
+var lookAhead = require("./lookAhead");
+
+
+function exactFit(rectangles){
+	var bestLayout = null;
+
+	function captureLayout(_, rectangles){
+		bestLayout = rectangles.map(function(rect){
+			return {x: rect.x, y: rect.y, i: rect.i};
+		});
+	}
+
+	var stack = [{envelope: new Envelope(), area: 0, index: 0}];
+
+	var bestScore = lookAhead(rectangles, stack, rectangles.length, captureLayout);
+
+	return {
+		score:  bestScore,
+		layout: bestLayout
+	};
+}
+
+module.exports = lookAhead;
